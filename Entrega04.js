592742f0807a8beb74c4fa95de2c69f498d514e6
gentlemankevs <kevin.ribeiro5@etec.sp.gov.br>
2015-09-07 12:49:21 -0300

    Entrega 4

---
+++ Entrega04.js
@@ -0,0 +1,40 @@
+
+function Animal(barulho){
+  this.barulho = barulho;
+};
+
+var animalPrototipo = {fazerBarulho :function (){
+  return this.barulho;
+}};
+
+function Manada(Animal){
+		var animais = [];
+    };
+	
+var manadaPrototipo = {addAnimal : function(Animal){
+	animais.push(Animal);
+}};
+
+function manadaVirgula(animais){
+	var barulheira = '';
+}
+var manadaVirgulaPrototipo = {mostrarBarulheira: function(animais){
+		for (i = 0; i<=animais.lenght; i++){
+		if (i===animais.lenght){
+		barulheira+=bicho.fazerBarulho;
+		}
+		else{
+		barulheira+=bicho.fazerBarulho+', ';
+		}
+	}
+}}
+Animal.prototype = animalPrototipo;
+Manada.prototype = manadaPrototipo;
+manadaVirgula.prototype = manadaVirgulaPrototipo;
+manadaVirgulaPrototipo.prototype = manadaPrototipo;
+
+
+var Cachorro = new Animal('Au au');
+var Gato = new Animal('Miau');
+
+console.log(manadaVirgulaPrototipo.mostrarBarulheira());
